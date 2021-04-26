d88edba1945b78288edd3f17e102e814f08c3575
hafizadit <80144805+hafizadit@users.noreply.github.com>
2021-04-26 09:32:54 +0700

    Add files via upload

---
+++ Diamond.py
@@ -0,0 +1,41 @@
+import math
+import sys
+from rich.console import Console
+
+console = Console()
+
+print("")
+console.print("="*50,style="red")
+teks = "Program Diamond"
+console.print(teks.center(50),style="bold red")
+console.print("="*50,style="red")
+print("")
+
+n = " "
+while n != "":
+    n = input("\nmasukkan angka ganjil :")
+    print("")
+    y = 1
+    x = 0
+    c = 4
+
+    if n != "":
+        for i in range(int(n)):
+            if i < math.ceil(int(n)/2):
+                console.print(" "*(int(n)-i),"*"*(y+x),style="cyan")
+                y += 1
+                x += 1
+            else:
+                console.print(" "*(x+1),"*"*(y+x-c),style="cyan")
+                c += 4
+                x += 1
+                y += 1
+    else:
+        print("")
+        console.print("="*50,style="red")
+        teks = "Program Selesai"
+        console.print(teks.center(50),style="bold red")
+        console.print("="*50,style="red")
+        print("")
+        sys.exit()
+
